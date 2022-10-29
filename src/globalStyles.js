05e5559163f04356b2fff3115a140ab439d4f138
FerLagHer <flagunahermida@gmail.com>
2022-10-28 21:03:21 -0300

    Font fix 5

--- src/globalStyles.js
+++ src/globalStyles.js
@@ -1,19 +1,19 @@
 import nukuEot from './assets/fonts/nukunuku.eot';
 import nukuWoff2 from './assets/fonts/nukunuku.woff2';
 import nukuWoff from './assets/fonts/nukunuku.woff';
-import nukuTtF from './assets/fonts/nukunuku.ttf';
+import nukuTtf from './assets/fonts/nukunuku.ttf';
 import nukuSvg from './assets/fonts/nukunuku.svg';
 import { createGlobalStyle } from 'styled-components';
 
 export const GlobalStyle = createGlobalStyle`
 @font-face {
     font-family: 'Nuku Nuku';
-    src: url(nukuEot);
-    src: url(nukuEot + '?#iefix') format('embedded-opentype'),
-        url(nukuWoff2) format('woff2'),
-        url(nukuWoff) format('woff'),
-        url(nukuTtf) format('truetype'),
-        url(nukuSvg) format('svg');
+    src: url(${nukuEot});
+    src: url(${nukuEot} + '?#iefix') format('embedded-opentype'),
+        url(${nukuWoff2}) format('woff2'),
+        url(${nukuWoff}) format('woff'),
+        url(${nukuTtf}) format('truetype'),
+        url(${nukuSvg}) format('svg');
     font-weight: normal;
     font-style: normal;
 }
