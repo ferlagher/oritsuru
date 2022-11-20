c5322229cc988d1e91811830bf84c43a9d103f29
FerLagHer <flagunahermida@gmail.com>
2022-11-20 20:11:03 -0300

    add Toggle component

--- src/components/FavToggle/FavToggle.js
+++ src/components/FavToggle/FavToggle.js
@@ -3,7 +3,6 @@ import { useFavToggle } from "./useFavToggle";
 
 export const FavToggle = ({id}) => {
     const [isInStorage, toggleFav] = useFavToggle(id);
-    console.log("🚀 ~ file: FavToggle.js ~ line 6 ~ FavToggle ~ isInStorage", isInStorage)
 
     return(
         <Toggle value={isInStorage} onToggle={toggleFav}>
