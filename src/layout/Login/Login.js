1ff710cc19a4b3d4f9daeed881338c376c6d11cf
FerLagHer <flagunahermida@gmail.com>
2022-11-22 03:07:23 -0300

    Login config

--- src/layout/Login/Login.js
+++ src/layout/Login/Login.js
@@ -4,13 +4,19 @@ import { useEffect, useState } from "react";
 const URL = `/.netlify/functions/authentication?email=a@h.com&pass=1234&action=login`;
 
 const getAuth = async () => {
-    const {token} = fetch(URL).then(res => res.json());
-    return token;
+    try {
+        const {token} = fetch(URL).then(res => res.json());
+        console.log("🚀 ~ file: Login.js ~ line 9 ~ getAuth ~ token", token)
+
+        return token;
+    } catch(err) {
+        console.error(err);
+    }
 }
 
 export const Login = () => {
     const [user, setUser] = useState('');
-    setUser(setUser(getAuth()))
+    setUser(setUser(getAuth()));
     useEffect(() => console.log(user), [user])
     return(<section></section>);
 };
